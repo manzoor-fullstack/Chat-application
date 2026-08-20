3c14702934cb7e19548a867e85e28f27e99dd239
Manzoor Ahmed <manzoorjam467@gmail.com>
2026-08-20 16:12:24 +0500

    feat(api): configure Prisma and PostgreSQL

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from './config/env.validation';
+import { PrismaModule } from './database/prisma.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidationSchema,
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,54 @@
+/* eslint-disable prettier/prettier */
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+
+  app.setGlobalPrefix('api');
+
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: configService.getOrThrow<string>('CLIENT_URL'),
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('SyncSpace API')
+    .setDescription('API documentation for the SyncSpace chat application.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
+  const port = configService.getOrThrow<number>('PORT');
+
+  await app.listen(port);
+
+  console.log(`🚀 API running on http://localhost:${port}`);
+  console.log(
+    `📚 Swagger running on http://localhost:${port}/api/docs`,
+  );
 }
+
 bootstrap();
